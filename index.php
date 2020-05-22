41f650d48d948124ac015d300e157844d9cd6639
mapgh <soporte@maphg.com>
2020-05-22 17:16:45 -0500

    test

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-echo "Hola mundo! test de modificacion 2";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="svg/logo6.png">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
